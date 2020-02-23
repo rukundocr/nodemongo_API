a6c0cf2e526b2cedb7acbe488faf58c35fb6cc4b
rukundocr <rukundocr@gmail.com>
2020-02-24 01:14:09 +0200

    fixing issue with sesnsordata schema

--- model/sensor.js
+++ model/sensor.js
@@ -4,10 +4,10 @@ const thingschmema = mongoose.Schema({
     moisture: { type: String, required: true },
     temp: { type:String, required: true },
     //userId: { type: String, required: true },
-    humidity: { type: Number, required: true },
+    humidity: { type: String, required: true },
     motor_status:{type:Number,required:true},
    // created_at:{type:new Date(), required:true}
-    updated: { type: Date, default: Date.now() },
+    updated: { type: Date, default: Date.now(), required:false},
 });
 
 module.exports = mongoose.model('SensorDta',thingschmema);
